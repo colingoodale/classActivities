dc63bad0939541412d665e0a6aeff9b7d7671770
Colin <colingoodale@gmail.com>
2018-08-01 18:50:21 -0500

    notes for today

---
+++ week-2/day-3/Activities/29-JoanOfArcArrays/Unsolved/joan-of-arc-solved.js
@@ -0,0 +1,10 @@
+var joanofArchInfo = {
+    "Real Name": "Jehanne la Purcelle.",
+    "Grew Up Where": "Domremy, a village in northeastern France",
+    "Known For": "Tearin Shit Up",
+    "Scars": "One from an Arrow and one from a Spear in Paris",
+    "Symbolism": "Pretty symbolic."
+};
+
+//type in info for auto fill
+
