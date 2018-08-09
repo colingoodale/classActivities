f71cf6a4c307e1a95a96ee5514673e92b87d8531
Colin <colingoodale@gmail.com>
2018-08-09 13:35:49 -0500

    midday notes

---
+++ 99-other-activities/function.js
@@ -0,0 +1,27 @@
+//create a function
+//named difference
+//takes inputs (numeric)
+//outputs the differences of the inputs
+
+function difference(x, y) {
+    return x - y;
+}
+
+console.log(difference(1, 5))
+
+//create a function
+//names wholeName
+//takes thee inputs (1, 2, 3)
+//outputs a sting with all three inputs concatenate
+//with a space between each
+
+function wholeName(first, middle, last) {
+    return first + "" + middle + '' + last;
+}
+
+var result = wholeName("Jorge", "Constant", "hernandez");
+console.log(result);
+
+//create a function
+//that takes one imput (string)
+//outputs the reverse of the input
