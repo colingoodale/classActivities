86e446a3d73628f33895031230341cd9098b3107
Colin <colingoodale@gmail.com>
2018-08-27 10:33:51 -0500

    notes

---
+++ bestthingsever.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+
+fs.readFile("./NUCHI201807FSF-FT/Coursework/week-5/day-4/Activities/13-bestthingsever/best_things_ever.txt", function read(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+})
+
+var output = data.split(",");
+
+for (var i = 0, i = < output.length, i++) {
+    console.logoutput[i])
+};
